be26453f6b1fe5c99caf45b38300e0d8f53e6823
Connor Finnell <connor.finnell@udacity.com>
2020-09-02 16:14:12 -0600

    feat: Allow missing authors, image

--- src/Book.js
+++ src/Book.js
@@ -3,34 +3,38 @@ import PropTypes from "prop-types";
 import BookShelfChanger from "./BookShelfChanger";
 
 export function objectIsBook(meta) {
-  return (
-    meta.id &&
-    meta.title &&
-    meta.authors &&
-    meta.imageLinks &&
-    meta.imageLinks.thumbnail
-  );
+  return meta.id && meta.title;
+}
+
+function getBookCoverStyle(meta) {
+  let style = {
+    width: 128,
+    height: 193,
+  };
+  if (meta.imageLinks && meta.imageLinks.thumbnail) {
+    style.backgroundImage = `url("${meta.imageLinks.thumbnail}")`;
+  } else {
+    style.backgroundColor = "grey";
+  }
+  return style;
 }
 
 function Book(props) {
   return (
     <div className="book">
       <div className="book-top">
-        <div
-          className="book-cover"
-          style={{
-            width: 128,
-            height: 193,
-            backgroundImage: `url("${props.meta.imageLinks.thumbnail}")`,
-          }}
-        />
+        <div className="book-cover" style={getBookCoverStyle(props.meta)} />
         <BookShelfChanger
           changeShelf={(shelf) => props.changeShelf(props.meta, shelf)}
           currentShelf={props.getCurrentShelf(props.meta.id)}
         />
       </div>
       <div className="book-title">{props.meta.title}</div>
-      <div className="book-authors">{props.meta.authors.join(", ")}</div>
+      {props.meta.authors ? (
+        <div className="book-authors">{props.meta.authors.join(", ")}</div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
